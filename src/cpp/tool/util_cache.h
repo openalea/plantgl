4259c990ac9f0800391ec7de5400dd05160d0eb4
Frederic Boudon <frederic.boudon@cirad.fr>
2007-01-30 10:32:52 +0000

    made a copy
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk/PlantGL@2556 ab253cce-29fb-0310-bb2f-979600cdbdeb

---
+++ src/cpp/tool/util_cache.h
@@ -0,0 +1,159 @@
+/* -*-c++-*-
+ *  ----------------------------------------------------------------------------
+ *
+ *       AMAPmod: Exploring and Modeling Plant Architecture
+ *
+ *       Copyright 1995-2000 UMR Cirad/Inra Modelisation des Plantes
+ *
+ *       File author(s): C. Nouguier & F. Boudon (frederic.boudon@cirad.fr) nouguier
+ *
+ *       $Source$
+ *       $Id$
+ *
+ *       Forum for AMAPmod developers    : amldevlp@cirad.fr
+ *
+ *  ----------------------------------------------------------------------------
+ *
+ *                      GNU General Public Licence
+ *
+ *       This program is free software; you can redistribute it and/or
+ *       modify it under the terms of the GNU General Public License as
+ *       published by the Free Software Foundation; either version 2 of
+ *       the License, or (at your option) any later version.
+ *
+ *       This program is distributed in the hope that it will be useful,
+ *       but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *       MERCHANTABILITY or FITNESS For A PARTICULAR PURPOSE. See the
+ *       GNU General Public License for more details.
+ *
+ *       You should have received a copy of the GNU General Public
+ *       License along with this program; see the file COPYING. If not,
+ *       write to the Free Software Foundation, Inc., 59
+ *       Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ *  ----------------------------------------------------------------------------
+ */
+
+
+/*! \file util_cache.h
+    \brief Definition of the container class Cache.
+*/
+
+#ifndef __util_cache_h__
+#define __util_cache_h__
+
+/* ----------------------------------------------------------------------- */
+
+#include "Tools/util_hashmap.h"
+
+/* ----------------------------------------------------------------------- */
+
+#include "geom_namespace.h"
+GEOM_BEGIN_NAMESPACE
+
+/* ----------------------------------------------------------------------- */
+
+/**
+   \class Cache
+   \brief A cache
+*/
+
+/* ----------------------------------------------------------------------- */
+
+template <class T>
+class Cache {
+
+public:
+
+  /// A const iterator used to iterate through the cache.
+#ifndef WIN32_STL_EXTENSION
+  typedef typename STDEXT::hash_map<uint32_t,T,STDEXT::hash<uint32_t>,std::equal_to<uint32_t> >::const_iterator \
+                                                                const_Iterator;
+#else
+  typedef typename STDEXT::hash_map<uint32_t,T>::const_iterator const_Iterator;
+#endif
+  /// An iterator used to iterate through the cache.
+
+#ifndef WIN32_STL_EXTENSION
+  typedef typename STDEXT::hash_map<uint32_t,T,STDEXT::hash<uint32_t>,std::equal_to<uint32_t> >::iterator Iterator;
+#else
+  typedef typename STDEXT::hash_map<uint32_t,T>::iterator Iterator;
+#endif
+
+  /// Constructs an empty Cache.
+  Cache( ) :
+    __cache() {
+  }
+
+  /// Destructor.
+  ~Cache( ) {
+    clear();
+  }
+
+  /// Returns an iterator at the beginning of \e self.
+  inline Iterator begin( ) {
+    return __cache.begin();
+  }
+
+  /// Returns an iterator at the beginning of \e self.
+  inline const_Iterator begin( ) const {
+    return __cache.begin();
+  }
+
+  /// Clears the cache.
+  inline void clear( ) {
+    __cache.clear();
+  }
+
+  /// Returns a const iterator at the beginning of \e self.
+  inline Iterator end( ) {
+    return __cache.end();
+  }
+
+  /// Returns a const iterator at the beginning of \e self.
+  inline const_Iterator end( ) const {
+    return __cache.end();
+  }
+
+  /// Returns an iterator to the object identified with \e id.
+  inline Iterator find( uint32_t id ) {
+    return __cache.find(id);
+  }
+
+  /** Inserts into \e self the element \e t associated to the object
+      identified with \e id. */
+  inline Iterator insert( uint32_t id, const T& t ) {
+    return __cache.insert(std::pair<uint32_t,T>(id,t)).first;
+  }
+    
+  inline void remove( uint32_t id ) {
+	Iterator _it = find(id);
+	if(_it != end()) __cache.erase(_it);
+  }
+
+  /// Returns whether \e self is empty.
+  inline bool isEmpty( ) const {
+    return __cache.empty();
+  }
+
+protected:
+
+  /// The elements contained by \e self.
+#ifndef WIN32_STL_EXTENSION
+  STDEXT::hash_map<uint32_t,T,STDEXT::hash<uint32_t>,std::equal_to<uint32_t> > __cache;
+#else
+  STDEXT::hash_map<uint32_t,T> __cache;
+#endif
+};
+
+
+
+/* ----------------------------------------------------------------------- */
+
+GEOM_END_NAMESPACE
+
+/* ----------------------------------------------------------------------- */
+// __util_cache_h__
+#endif
+
+
