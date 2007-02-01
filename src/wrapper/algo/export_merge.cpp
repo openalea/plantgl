7eb319371b106285f1d557a29eb1c8a84be2bcfc
Frederic Boudon <frederic.boudon@cirad.fr>
2007-02-01 08:41:38 +0000

    made a copy
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk/PlantGL@2597 ab253cce-29fb-0310-bb2f-979600cdbdeb

---
+++ src/wrapper/algo/export_merge.cpp
@@ -0,0 +1,64 @@
+/* -*-c++-*-
+ *  ----------------------------------------------------------------------------
+ *
+ *       GeomPy: Python wrapper for the Plant Graphic Library
+ *
+ *       Copyright 1995-2003 UMR AMAP 
+ *
+ *       File author(s): C. Pradal (christophe.pradal@cirad.fr)
+ *
+ *       $Source$
+ *       $Id$
+ *
+ *       Forum for AMAPmod developers    : amldevlp@cirad.fr
+ *
+ *  ----------------------------------------------------------------------------
+ *
+ *                      GNU General Public Licence
+ *
+ *       This program is free software; you can redistribute it and/or
+ *       modify it under the terms of the GNU General Public License as
+ *       published by the Free Software Foundation; either version 2 of
+ *       the License, or (at your option) any later version.
+ *
+ *       This program is distributed in the hope that it will be useful,
+ *       but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *       MERCHANTABILITY or FITNESS For A PARTICULAR PURPOSE. See the
+ *       GNU General Public License for more details.
+ *
+ *       You should have received a copy of the GNU General Public
+ *       License along with this program; see the file COPYING. If not,
+ *       write to the Free Software Foundation, Inc., 59
+ *       Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ *  ----------------------------------------------------------------------------
+ */
+
+#include "merge.h"
+
+#include <boost/python.hpp>
+
+#include "algo_merge.h"
+#include "actn_discretizer.h"
+
+#include "macro_refptr.h"
+#include "macro_property.h"
+
+GEOM_USING_NAMESPACE
+TOOLS_USING_NAMESPACE
+using namespace boost::python;
+using namespace std;
+
+SETGET(Merge,Model,ExplicitModelPtr)
+
+void class_Merge()
+{
+  class_< Merge, boost::noncopyable >
+    ("Merge", init<Discretizer&,ExplicitModelPtr&>("Merge(Discretizer d, ExplicitModel e )" ))
+	.DEC_SETGET(model,Merge,Model,ExplicitModelPtr)
+	.def("apply", ( bool(Merge::*)(GeometryPtr&)      ) &Merge::apply)
+	.def("apply", ( bool(Merge::*)(ExplicitModelPtr&) ) &Merge::apply)
+    ;
+}
+
+
