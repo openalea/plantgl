1cad2676b2e0c9d46f25b663c036e385d796eeaf
Frederic Boudon <frederic.boudon@cirad.fr>
2007-02-08 08:19:25 +0000

    made a copy
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk/PlantGL@2658 ab253cce-29fb-0310-bb2f-979600cdbdeb

---
+++ src/wrapper/scenegraph/export_transformation.cpp
@@ -0,0 +1,230 @@
+/* -*-c++-*-
+ *  ----------------------------------------------------------------------------
+ *
+ *       GeomPy: Python wrapper for the Plant Graphic Library
+ *
+ *       Copyright 1995-2003 UMR AMAP 
+ *
+ *       File author(s): C. Pradal (christophe.pradal@cirad.fr)
+ *
+ *       $Source$
+ *       $Id$
+ *
+ *       Forum for AMAPmod developers    : amldevlp@cirad.fr
+ *
+ *  ----------------------------------------------------------------------------
+ *
+ *                      GNU General Public Licence
+ *
+ *       This program is free software; you can redistribute it and/or
+ *       modify it under the terms of the GNU General Public License as
+ *       published by the Free Software Foundation; either version 2 of
+ *       the License, or (at your option) any later version.
+ *
+ *       This program is distributed in the hope that it will be useful,
+ *       but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *       MERCHANTABILITY or FITNESS For A PARTICULAR PURPOSE. See the
+ *       GNU General Public License for more details.
+ *
+ *       You should have received a copy of the GNU General Public
+ *       License along with this program; see the file COPYING. If not,
+ *       write to the Free Software Foundation, Inc., 59
+ *       Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ *  ----------------------------------------------------------------------------
+ */
+
+#include "transformation.h"
+
+#include <boost/python.hpp>
+
+#include <geom_transformed.h>
+#include <geom_mattransformed.h>
+#include <geom_tapered.h>
+#include <geom_axisrotated.h>
+#include <geom_eulerrotated.h>
+#include <geom_oriented.h>
+#include <geom_translated.h>
+#include <geom_scaled.h>
+#include <geom_pointarray.h>
+#include <boost/python/make_constructor.hpp>
+
+#include "macro_refptr.h"
+
+GEOM_USING_NAMESPACE
+TOOLS_USING_NAMESPACE
+using namespace boost::python;
+using namespace std;
+
+
+DEF_POINTEE(Transformation)
+DEF_POINTEE(Transformation2D)
+DEF_POINTEE(Transformation3D)
+DEF_POINTEE(Matrix3Transformation)
+DEF_POINTEE(Matrix4Transformation)
+DEF_POINTEE(OrthonormalBasis3D)
+DEF_POINTEE(Deformation)
+
+DEF_POINTEE(Taper)
+DEF_POINTEE(Scaling)
+DEF_POINTEE(Translation)
+DEF_POINTEE(AxisRotation)
+DEF_POINTEE(EulerRotation)
+DEF_POINTEE(BaseOrientation)
+DEF_POINTEE(Transform4)
+
+void class_PureTransformation()
+{
+  class_< Transformation, TransformationPtr, boost::noncopyable >("Transformation", no_init);
+
+  class_< Transformation2D, Transformation2DPtr, bases< Transformation >, boost::noncopyable  >
+    ("Transformation2D", no_init);
+  class_< Transformation3D, Transformation3DPtr, bases< Transformation >, boost::noncopyable  >
+    ("Transformation3D", no_init);
+  class_< Matrix3Transformation, Matrix3TransformationPtr, bases< Transformation2D >, boost::noncopyable  >
+    ("Matrix3Transformation", no_init);
+  class_< Matrix4Transformation, Matrix4TransformationPtr, bases< Transformation3D >, boost::noncopyable  >
+    ("Matrix4Transformation", no_init);
+  class_< Deformation, DeformationPtr, bases< Transformation3D >, boost::noncopyable  >
+    ("Deformation", no_init);
+
+  implicitly_convertible<Transformation2DPtr, TransformationPtr >();
+  implicitly_convertible<Transformation3DPtr, TransformationPtr >();
+  implicitly_convertible<Matrix3TransformationPtr, Transformation2DPtr >();
+  implicitly_convertible<Matrix4TransformationPtr, Transformation3DPtr >();
+  implicitly_convertible<DeformationPtr, Transformation3DPtr >();
+
+}
+
+void class_Taper()
+{
+  class_< Taper, bases< Deformation > , TaperPtr, boost::noncopyable >
+    ("Taper", init< const real_t&, const real_t&>() )
+	.def("getBaseRadius",&Taper::getBaseRadius, return_value_policy<copy_const_reference>())
+	.def("getTopRadius",&Taper::getTopRadius);
+
+  implicitly_convertible<TaperPtr, DeformationPtr>();
+}
+
+
+void class_Scaling()
+{
+  class_< Scaling, bases< Matrix4Transformation > , ScalingPtr, boost::noncopyable >
+    ("Scaling", init< const Vector3& >() );
+
+  implicitly_convertible<ScalingPtr, Matrix4TransformationPtr>();
+}
+
+void class_Translation()
+{
+  class_< Translation, bases< Matrix4Transformation > , TranslationPtr, boost::noncopyable >
+    ("Translation", init< const Vector3& >() );
+
+  implicitly_convertible<TranslationPtr, Matrix4TransformationPtr>();
+}
+
+
+void class_OrthonormalBasis3D()
+{
+  class_< OrthonormalBasis3D, bases< Matrix4Transformation > , OrthonormalBasis3DPtr, boost::noncopyable >
+    ob ("OrthonormalBasis3D", init< const Matrix3& >() );
+	ob.def("getMatrix3",&OrthonormalBasis3D::getMatrix3);
+	ob.def("getMatrix",&OrthonormalBasis3D::getMatrix);
+
+  implicitly_convertible<OrthonormalBasis3DPtr, Matrix4TransformationPtr>();
+}
+
+void class_AxisRotation()
+{
+  class_< AxisRotation, bases< OrthonormalBasis3D > , AxisRotationPtr, boost::noncopyable >
+    ("AxisRotation", init< const Vector3&, const real_t&>() );
+
+  implicitly_convertible<AxisRotationPtr, OrthonormalBasis3DPtr>();
+}
+
+void class_EulerRotation()
+{
+  class_< EulerRotation, bases< OrthonormalBasis3D > , EulerRotationPtr, boost::noncopyable >
+    ("EulerRotation", init< const real_t&, const real_t&, const real_t&>() );
+
+  implicitly_convertible<EulerRotationPtr, OrthonormalBasis3DPtr>();
+}
+
+void class_BaseOrientation()
+{
+  class_< BaseOrientation, bases< OrthonormalBasis3D > , BaseOrientationPtr, boost::noncopyable >
+    ("BaseOrientation", init< const Vector3&, const Vector3&>() );
+
+  implicitly_convertible<EulerRotationPtr, OrthonormalBasis3DPtr>();
+}
+
+
+void translate( Transform4& t, const TOOLS(Vector3)& v ) { t.translate(v); }
+void scale( Transform4& t, const TOOLS(Vector3)& v ) { t.scale(v); }
+void rotate_t4( Transform4& t, const TOOLS(Matrix3)& v ) { t.rotate(v); }
+
+Transform4Ptr t4_default()
+{ return new Transform4();}
+
+std::string t4_str( Transform4* t )
+{
+  std::stringstream ss;
+  ss << t->getMatrix();
+  return ss.str();
+}
+
+std::string t4_repr( Transform4 * a )
+{
+  std::stringstream ss;
+  Matrix4 m=a->getMatrix();
+  real_t* data= m.getData();
+  ss << "Transform4(Matrix4( ";
+  std::copy( data, data+15, std::ostream_iterator< real_t >( ss, ", " ) );
+  ss << *(data+15) << "])";
+  return ss.str();
+}
+
+Point3ArrayPtr t4_transform_p3( Transform4 * t, const Point3ArrayPtr& points )
+{
+  return t->transform(points);
+}
+
+tuple t4_getTransformation(Transform4 * t)
+{
+	Vector3 scale,rotate,translate;
+	t->getTransformation(scale,rotate,translate);
+	return make_tuple(scale,rotate,translate);
+}
+
+void class_Transform4()
+{
+  class_< Transform4, Transform4Ptr, bases< Matrix4Transformation > , boost::noncopyable > 
+    m2("Transform4", init<const Matrix4&>() );
+  m2.def("__init__", make_constructor(t4_default));
+  m2.def("isValid", &Transform4::isValid);
+  m2.def("translate", translate,"Add a translation at the transformation");
+  m2.def("scale", scale,"Add a scaling at the transformation");
+  m2.def("rotate", rotate_t4,"Add a rotation at the transformation");
+  m2.def("getTransformation", &t4_getTransformation,"Return scale, rotate and translate corresponding values.");
+  m2.def("getVolume", &Transform4::getVolume,"compute the Matrix3 determinant of the transformation");
+  m2.def("transform", t4_transform_p3, "transform a set of points");
+  m2.def("__str__",t4_str);
+  m2.def("__repr__",t4_repr);
+
+  implicitly_convertible<Transform4Ptr, Matrix4TransformationPtr>();
+
+}
+
+void class_Transformation()
+{
+  class_PureTransformation();
+  class_Taper();
+  class_Scaling();
+  class_Translation();
+  class_OrthonormalBasis3D();
+  class_AxisRotation();
+  class_EulerRotation();
+  class_BaseOrientation();
+  class_Transform4();
+
+}
