6c0da2718061fa1980becb5044d9c2b884313871
Frederic Boudon <frederic.boudon@cirad.fr>
2007-02-08 08:11:44 +0000

    made a copy
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk/PlantGL@2640 ab253cce-29fb-0310-bb2f-979600cdbdeb

---
+++ src/wrapper/scenegraph/export_extrusion.cpp
@@ -0,0 +1,58 @@
+#include "extrusion.h"
+
+#include <geom_extrusion.h>
+#include <geom_lineicmodel.h>
+#include <geom_pointarray.h>
+#include <geom_profile.h>
+#include <geom_curve.h>
+
+#include "macro_refptr.h"
+#include "macro_property.h"
+
+#include <boost/python.hpp>
+#include <string>
+#include <sstream>
+
+GEOM_USING_NAMESPACE
+TOOLS_USING_NAMESPACE
+
+using namespace boost::python;
+
+DEF_POINTEE( Extrusion )
+
+SETGET(Extrusion,Axis,LineicModelPtr);
+SETGET(Extrusion,CrossSection,Curve2DPtr);
+
+SETGET(Extrusion,Solid,bool);
+SETGET(Extrusion,CCW,bool);
+
+#define SETGET2(_CLASS,PROP,TYPE)\
+TYPE get_##_CLASS##_##PROP(_CLASS * obj){ \
+  return obj->getProfileTransformation()->get##PROP(); \
+} \
+void set_##_CLASS##_##PROP(_CLASS * obj, TYPE v){ \
+  obj->getProfileTransformation()->get##PROP() = v; \
+} \
+
+SETGET2(Extrusion,Scale,Point2ArrayPtr);
+SETGET2(Extrusion,Orientation,RealArrayPtr);
+SETGET2(Extrusion,KnotList,RealArrayPtr);
+
+
+void class_Extrusion()
+{
+  class_<Extrusion, ExtrusionPtr, bases<ParametricModel>, boost::noncopyable>
+	( "Extrusion", init<LineicModelPtr,Curve2DPtr,optional<Point2ArrayPtr> >
+	 ( "Extrusion (LineicModel, Curve2D,[Point2Array([])])"))
+    .def( "copy", &Extrusion::copy )
+	.DEC_SETGET(axis,Extrusion,Axis,LineicModelPtr)
+	.DEC_SETGET(crossSection,Extrusion,CrossSection,Curve2DPtr)
+	.DEC_SETGET(scale,Extrusion,Scale,Point2ArrayPtr)
+	.DEC_SETGET(orientation,Extrusion,Orientation,RealArrayPtr)
+	.DEC_SETGET_WD(solid,Extrusion,Solid,bool)
+	.DEC_SETGET_WD(ccw, Extrusion,CCW,bool)
+    ;
+  implicitly_convertible<ExtrusionPtr, ParametricModelPtr>();
+}
+
+
