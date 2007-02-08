28dc5caa936473e633ff61f48dc73e1da9ee691a
Frederic Boudon <frederic.boudon@cirad.fr>
2007-02-08 08:15:08 +0000

    made a copy
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk/PlantGL@2649 ab253cce-29fb-0310-bb2f-979600cdbdeb

---
+++ src/wrapper/scenegraph/export_pointarrays.cpp
@@ -0,0 +1,53 @@
+#include "pointarrays.h"
+#include "macro_refptr.h"
+#include "vector.h"
+
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <iterator>
+#include <algorithm>
+
+#include <util_array.h>
+#include <util_tuple.h>
+#include <geom_pointarray.h>
+#include <geom_ifs.h>
+#include <geom_mattransformed.h>
+#include <boost/python.hpp>
+#include <boost/python/make_constructor.hpp>
+
+#include "exception.hh"
+
+GEOM_USING_NAMESPACE
+TOOLS_USING_NAMESPACE
+
+using namespace boost::python;
+
+#include "arrays_macro.h"
+
+EXPORT_FUNCTION( p2a, Vector2, Point2Array )
+EXPORT_FUNCTION( p3a, Vector3, Point3Array )
+EXPORT_FUNCTION( p4a, Vector4, Point4Array )
+EXPORT_FUNCTION( t4a, Transform4Ptr, Transform4Array )
+EXPORT_FUNCTION( m4a, Matrix4, Matrix4Array )
+
+EXPORT_NUMPY( p2a, Vector2, Point2Array, 0, 2, real_t )
+EXPORT_NUMPY( p3a, Vector3, Point3Array, 0, 3, real_t )
+EXPORT_NUMPY( p4a, Vector4, Point4Array, 0, 4, real_t )
+
+void class_pointarrays()
+{
+  // define_stl_exceptions();
+
+  EXPORT_ARRAY( p2a, Point2Array, "Point2Array([Vector2(x,y),...])")
+    DEFINE_NUMPY( p2a );
+  EXPORT_ARRAY( p3a, Point3Array, "Point3Array([Vector3(x,y,z),...])")
+    DEFINE_NUMPY( p3a );
+  EXPORT_ARRAY( p4a, Point4Array, "Point4Array([Vector4(x,y,z,w),...])")
+    DEFINE_NUMPY( p4a );
+  EXPORT_ARRAY( t4a, Transform4Array,"Transform4Array([Transform4(...),...])" );
+  EXPORT_ARRAY( m4a, Matrix4Array,"Matrix4Array([Matrix4(...),...])" );
+}
+
+
+
