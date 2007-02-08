e39e207ee10b918c3213b9b8f3f4d09cf85c0cd2
Frederic Boudon <frederic.boudon@cirad.fr>
2007-02-08 08:18:50 +0000

    made a copy
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk/PlantGL@2657 ab253cce-29fb-0310-bb2f-979600cdbdeb

---
+++ src/wrapper/scenegraph/export_text.cpp
@@ -0,0 +1,66 @@
+#include "text.h"
+
+#include <geom_text.h>
+
+#include <boost/python.hpp>
+#include <string>
+
+#include "macro_refptr.h"
+#include "macro_property.h"
+
+
+using namespace boost::python;
+
+GEOM_USING_NAMESPACE
+TOOLS_USING_NAMESPACE
+
+DEF_POINTEE(Font)
+
+SETGET(Font,Family,std::string);
+SETGET(Font,Size,uint32_t);
+SETGET(Font,Bold,bool);
+SETGET(Font,Italic,bool);
+
+void class_Font()
+{
+  class_< Font, FontPtr, bases< SceneObject >,boost::noncopyable >
+    ("Font",init<optional<std::string,int,bool,bool> >
+     (args("family","size","bold","italic"),
+	  "Font([family,size,bold,italic])"))
+    .def( "copy", &Font::copy )
+	.DEC_SETGET(family,Font,Family,std::string)
+	.DEC_SETGET_WD(size,Font,Size,uint32_t)
+	.DEC_SETGET_WD(bold,Font,Bold,boll)
+	.DEC_SETGET_WD(italic,Font,Italic,boll)
+
+    ;
+
+  implicitly_convertible< FontPtr, SceneObjectPtr >();
+}
+
+
+DEF_POINTEE(Text)
+
+SETGET(Text,String,std::string);
+SETGET(Text,FontStyle,FontPtr);
+SETGET(Text,Position,Vector3);
+SETGET(Text,ScreenCoordinates,bool);
+
+void class_Text()
+{
+  class_< Text, TextPtr, bases< Geometry >,boost::noncopyable >
+    ("Text",init<std::string,optional<const TOOLS(Vector3)&,bool, const FontPtr&> >
+     (args("string","position","screencoordinates","fontstyle"),
+	  "Text(str string[, Vector3 position, bool screencoordinates, Font fontstyle])"))
+    .def( "copy", &Text::copy )
+	.DEC_SETGET(string,Text,String,std::string)
+	.DEC_SETGET_WD(fontstyle,Text,FontStyle,FontPtr)
+	.DEC_SETGET_WD(position,Text,Position,Vector3)
+	.DEC_SETGET_WD(screencoordinates,Text,ScreenCoordinates,bool)
+
+    ;
+
+  implicitly_convertible< TextPtr,GeometryPtr >();
+  
+  class_Font();
+}
