3a133a0811bf1ec20ce820c1a52a655d310be294
Frederic Boudon <frederic.boudon@cirad.fr>
2007-02-08 08:16:49 +0000

    made a copy
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk/PlantGL@2652 ab253cce-29fb-0310-bb2f-979600cdbdeb

---
+++ src/wrapper/scenegraph/export_primitive.cpp
@@ -0,0 +1,103 @@
+/* -*-c++-*-
+ *  ----------------------------------------------------------------------------
+ *
+ *       GeomPy: Python wrapper for the Plant Graphic Library
+ *
+ *       Copyright 1995-2003 UMR AMAP 
+ *
+ *       File author(s): C. Pradal (christophe.pradal@cirad.fr)
+ *
+ *       $Source$
+ *       $Id$
+ *
+ *       Forum for AMAPmod developers    : amldevlp@cirad.fr
+ *
+ *  ----------------------------------------------------------------------------
+ *
+ *                      GNU General Public Licence
+ *
+ *       This program is free software; you can redistribute it and/or
+ *       modify it under the terms of the GNU General Public License as
+ *       published by the Free Software Foundation; either version 2 of
+ *       the License, or (at your option) any later version.
+ *
+ *       This program is distributed in the hope that it will be useful,
+ *       but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *       MERCHANTABILITY or FITNESS For A PARTICULAR PURPOSE. See the
+ *       GNU General Public License for more details.
+ *
+ *       You should have received a copy of the GNU General Public
+ *       License along with this program; see the file COPYING. If not,
+ *       write to the Free Software Foundation, Inc., 59
+ *       Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ *  ----------------------------------------------------------------------------
+ */
+
+#include "primitive.h"
+#include "mesh.h"
+#include "lineicmodel.h"
+
+#include "extrusion.h"
+#include "patch.h"
+#include "hull.h"
+#include "revolution.h"
+#include "hull.h"
+
+#include <boost/python.hpp>
+
+#include <scne_sceneobject.h>
+#include <geom_geometry.h>
+
+#include <util_vector.h>
+
+#include <geom_primitive.h>
+#include <geom_parametricmodel.h>
+#include <geom_box.h>
+
+
+#include "macro_refptr.h"
+
+GEOM_USING_NAMESPACE
+TOOLS_USING_NAMESPACE
+using namespace boost::python;
+using namespace std;
+
+DEF_POINTEE(Box)
+DEF_POINTEE(Primitive)
+DEF_POINTEE(ParametricModel)
+DEF_POINTEE(Geometry)
+
+void class_PurePrimitive()
+{
+  
+  class_< Primitive, PrimitivePtr, bases< Geometry >,boost::noncopyable >("Primitive",no_init);
+  class_< ParametricModel, ParametricModelPtr, bases< Primitive >,boost::noncopyable >("ParametricModel",no_init);
+
+  implicitly_convertible< PrimitivePtr, GeometryPtr >();
+  implicitly_convertible< ParametricModelPtr,PrimitivePtr >();
+}
+
+void class_Box()
+{
+  class_< Box, BoxPtr, bases< ParametricModel > , boost::noncopyable >
+    ("Box", init< const Vector3& >("Box(Vector3(x,y,z))") );
+
+  implicitly_convertible<BoxPtr, ParametricModelPtr >();
+}
+
+
+void class_Primitive()
+{
+  class_PurePrimitive();
+  class_ExplicitModel();
+  class_LineicModel();
+  class_PlanarModel();
+
+  class_Box();
+
+  class_Revolution();
+  class_Hull();
+  class_Extrusion();
+  class_Patch();
+}
