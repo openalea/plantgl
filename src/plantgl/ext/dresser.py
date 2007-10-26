3cdd3fd6a9f92c0a41f935aed0ac4a57ef9b7727
David Da Silva <dodasilva@ucdavis.edu>
2007-10-26 10:12:31 +0000

    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk/PlantGL@3983 ab253cce-29fb-0310-bb2f-979600cdbdeb

--- src/plantgl/ext/dresser.py
+++ src/plantgl/ext/dresser.py
@@ -41,7 +41,7 @@ from openalea.plantgl.ext.pgl_utils import sphere, arrow, color, createSwung
 houppier_material = pgl.Material("houppier_mat",pgl.Color3(20,100,60))
 trunk_material = pgl.Material("trunk_material",pgl.Color3(50,28,6),2)
 
-from math import pi
+from math import pi, cos, sin, radians
   
 def makeScene( trees, midCr = 0.5, wood=True, random=False):
   #treeList = treesFromFile(file)
@@ -76,6 +76,8 @@ def viewMesures( file):
 def asymetric_swung( obj , **kwds ):
   X_attr = kwds.get('X', 'X')
   Y_attr = kwds.get('Y', 'Y')
+  pos_dist = kwds.get('dist', 'Distance')
+  pos_az = kwds.get('az', 'Azimuth')
   circ_attr = kwds.get('circ_attr', 'Circonference')
   height_attr = kwds.get('height_attr', 'Haut')
   botHoup_attr = kwds.get('botHoup', 'BaseHoup')
@@ -90,14 +92,25 @@ def asymetric_swung( obj , **kwds ):
   az_kz.sort()
   assert( len(rd_kz) == len(az_kz) and "directional canopy length and azimuth are not the same size" )
 
-  houppier=[ ( obj.__dict__[rd_kz[i]], true_azimuth(obj.__dict__[az_kz[i]]) ) for i in range( len(rd_kz) ) ]
+  houppier=[ ( obj.__dict__[rd_kz[i]], houppier2geomAZ(obj.__dict__[az_kz[i]]) ) for i in range( len(rd_kz) ) ]
   houppier.sort(cmp = lambda x,y : cmp(x[1],y[1]))
   
+  try :
+    r = obj.__dict__[pos_dist]
+    a = obj.__dict__[pos_az]
+    objX = r*cos( radians(forest2geomAZ(a)) )
+    objY = r*sin( radians(forest2geomAZ(a)) )
+  except KeyError: 
+    objX = obj.__dict__[X_attr]
+    objY = obj.__dict__[Y_attr]
+    print "using cartesian coordinate", obj.Arbre
+
+    
   mc = float(midCrown)
   radii = list(houppier)
   ht = 100* (obj.__dict__[height_attr] - obj.__dict__[botHoup_attr])
-  h = pgl.Translated( pgl.Vector3(obj.__dict__[X_attr], obj.__dict__[Y_attr], obj.__dict__[botHoup_attr]*100), createSwung(ht*mc,ht,radii) )
-  tr = pgl.Translated(pgl.Vector3(obj.__dict__[X_attr], obj.__dict__[Y_attr], 0), pgl.Cylinder( obj.__dict__[circ_attr]/(2*pi), obj.__dict__[botHoup_attr]*100 + ht*0.1) )
+  h = pgl.Translated( pgl.Vector3(objX, objY, obj.__dict__[botHoup_attr]*100), createSwung(ht*mc,ht,radii) )
+  tr = pgl.Translated(pgl.Vector3(objX, objY, 0), pgl.Cylinder( obj.__dict__[circ_attr]/(2*pi), obj.__dict__[botHoup_attr]*100 + ht*0.1) )
   
   s_h = pgl.Shape(h, houppier_material, obj.pid)
   s_tr = pgl.Shape(tr, trunk_material, obj.pid+100000)
@@ -109,22 +122,42 @@ def asymetric_swung( obj , **kwds ):
 def true_azimuth(az):
   return 90 - ( ( az+200 )%400 )*0.9
 
+def forest2geomAZ(az):
+  return -az*0.9
+
+def houppier2geomAZ(az):
+  return  - ( ( az+200 )%400 )*0.9
+
 def chupa_chups( obj , **kwds ):
   X_attr = kwds.get('X_attr', 'X')
   Y_attr = kwds.get('Y_attr', 'Y')
+  pos_dist = kwds.get('dist', 'Distance')
+  pos_az = kwds.get('az', 'Azimuth')
   circ_attr = kwds.get('circ_attr', 'Circonference')
   height_attr = kwds.get('height_attr', 'Haut')
   botHoup_attr = kwds.get('botHoup', 'BaseHoup')
   radiusHoup = kwds.get('radiusHoup', None)
   wood = kwds.get( 'wood', True )
 
+  try :
+    r = obj.__dict__[pos_dist]
+    a = obj.__dict__[pos_az]
+    objX = r*cos( radians(forest2geomAZ(a)) )
+    objY = r*sin( radians(forest2geomAZ(a)) )
+    print "using polar coordinate"
+  except AttributeError: 
+    objX = obj.__dict__[X_attr]
+    objY = obj.__dict__[Y_attr]
+    print "using cartesian coordinate"
+
+
   ht = 100* (obj.__dict__[height_attr] - obj.__dict__[botHoup_attr])
   if radiusHoup :
     sph_radius = obj.__dict__[radiusHoup]
   else :
     sph_radius = ht/2.
-  h = pgl.Translated( pgl.Vector3(obj.__dict__[X_attr], obj.__dict__[Y_attr], obj.__dict__[botHoup_attr]*100 + sph_radius), pgl.Sphere(sph_radius,10,5) )
-  tr = pgl.Translated(pgl.Vector3(obj.__dict__[X_attr], obj.__dict__[Y_attr], 0), pgl.Cylinder( obj.__dict__[circ_attr]/(2*pi), obj.__dict__[botHoup_attr]*100 + ht*0.1) )
+  h = pgl.Translated( pgl.Vector3(objX, objY, obj.__dict__[botHoup_attr]*100 + sph_radius), pgl.Sphere(sph_radius,10,5) )
+  tr = pgl.Translated(pgl.Vector3(objX, objY, 0), pgl.Cylinder( obj.__dict__[circ_attr]/(2*pi), obj.__dict__[botHoup_attr]*100 + ht*0.1) )
   
   s_h = pgl.Shape(h, houppier_material, obj.pid)
   s_tr = pgl.Shape(tr, trunk_material, obj.pid+100000)
