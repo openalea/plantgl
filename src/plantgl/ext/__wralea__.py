d5e7896695ad22814da3341471ef36d950e0951b
Samuel Dufour-Kowalski <samuel.dufour@cirad.fr>
2008-07-31 15:42:09 +0000

    rename wralea for PlantGL dresser
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk/PlantGL@5374 ab253cce-29fb-0310-bb2f-979600cdbdeb

---
+++ src/plantgl/ext/__wralea__.py
@@ -0,0 +1,39 @@
+# -*- python -*-
+#
+#       OpenAlea.PlantGL.ext Library: PlantGL external Library module
+#
+#       Copyright 2006 INRIA - CIRAD - INRA  
+#
+#       File author(s): BOUDON Frederic <frederic.boudon@cirad.fr>
+#                       Da SILVA David <david.da_silva@cirad.fr>
+#
+#       Distributed under the Cecill-C License.
+#       See accompanying file LICENSE.txt or copy at
+#           http://www.cecill.info/licences/Licence_CeCILL-C_V1-en.html
+# 
+#       OpenAlea WebSite : http://openalea.gforge.inria.fr
+#
+
+
+from openalea.core import *
+
+__name__ = "vplants.plantgl.dresser"
+__alias__ = ["PlantGL.Dresser"]
+
+__version__ = '0.0.1',
+__license__ = 'CECILL-C',
+__authors__ = 'F. Boudon and D. Da Silva',
+__institutes__ = 'INRIA/CIRAD',
+__description__ = 'Spatial distribution module.',
+__url__ = 'http://www.scipy.org'
+    
+__all__ = ["dresser"]    
+
+dresser = Factory( name="PGL Dresser",
+                   description="Add Pgl geometry an object",
+                   category="Modelling",
+                   nodemodule="dresser",
+                   nodeclass="dresser",
+                   )
+                  
+
