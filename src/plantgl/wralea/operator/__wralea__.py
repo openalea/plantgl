a3b735f9b7711ddae03f57dab75429161e03529a
Samuel Dufour-Kowalski <samuel.dufour@cirad.fr>
2008-07-31 15:38:23 +0000

    refactor wralea files for PlantGL
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk/PlantGL@5373 ab253cce-29fb-0310-bb2f-979600cdbdeb

---
+++ src/plantgl/wralea/operator/__wralea__.py
@@ -0,0 +1,41 @@
+from openalea.core import *
+
+__name__ = "vplants.plantgl.operator"
+__alias__= ["Geometric Operator"]
+
+__version__ = '0.0.1',
+__license__ = 'CECILL-V2',
+__authors__ = 'C. Pradal',
+__institutes__ = 'INRIA/CIRAD',
+__description__ = 'PlantGL Object Generator nodes.',
+__url__ =  'http://openalea.gforge.inria.fr'
+
+__all__ = ['random_sample', 'regular_sample','planter', 'import_scene']
+
+random_sample = Factory( name= "random sample", 
+                         description= "Create random distribution", 
+                         category = "Stand", 
+                         nodemodule = "frog",
+                         nodeclass = "RandomSample",
+                         )
+
+regular_sample = Factory( name= "regular sample", 
+                          description= "Create regular distribution", 
+                          category = "Stand", 
+                          nodemodule = "frog",
+                          nodeclass = "RegularSample",
+                          )
+
+planter = Factory( name= "planter", 
+                   description= "Apply a set of transformation to a shape", 
+                   category = "Vizualisation", 
+                   nodemodule = "frog",
+                   nodeclass = "Planter",
+                   )
+    
+import_scene = Factory( name= "import scene", 
+                        description= "Import GEOM scene", 
+                        category = "Vizualisation", 
+                        nodemodule = "frog",
+                        nodeclass = "ImportScene",
+                        )
---
+++ src/plantgl/wralea/visualization/__wralea__.py
@@ -0,0 +1,30 @@
+from openalea.core import *
+
+
+__name__ = "vplants.plantgl.visualization"
+__alias__ = ["PlantGL.Visualization"]
+
+__version__ = '0.0.2',
+__license__ = 'CECILL-V2',
+__authors__ = 'F. Boudon',
+__institutes__ = 'INRIA/CIRAD',
+__description__ = 'PlantGL Object Generator nodes.',
+__url__ =  'http://openalea.gforge.inria.fr'
+
+__all__ = ['plot3d']
+        
+plot3d = Factory( name= "plot3D", 
+                  description= "Viewer Display", 
+                  category = "Vizualisation", 
+                  nodemodule = "viewernode",
+                  nodeclass = "Plot3D",
+                  #lazy = False
+                  )
+
+#    nf = Factory( name= "addToPlot3D", 
+#                  description= "Viewer Display", 
+#                  category = "Vizualisation", 
+#                  nodemodule = "viewernode",
+#                  nodeclass = "AddToPlot3D",
+#                  lazy = False
+#                  )
