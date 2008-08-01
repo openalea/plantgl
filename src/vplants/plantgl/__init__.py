69cd5cb21dcb9b59a04e09394fe4f7dde133ab87
Samuel Dufour-Kowalski <samuel.dufour@cirad.fr>
2008-08-01 15:04:05 +0000

    Fix __init__.py for vplants namespace
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk/PlantGL@5382 ab253cce-29fb-0310-bb2f-979600cdbdeb

--- src/vplants/plantgl/__init__.py
+++ src/vplants/plantgl/__init__.py
@@ -2,10 +2,6 @@
 import os
 import openalea.plantgl
 
-cdir = os.path.dirname(__file__)
-pdir = os.path.join(cdir, "../../plantgl")
-pdir = os.path.abspath(pdir)
-
 __path__ = openalea.plantgl.__path__ + __path__[:]
 
 from openalea.plantgl.__init__ import *
