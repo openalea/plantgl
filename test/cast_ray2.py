a0f4d38d1c86c1feed1f61d918e0dc8d0ebd7cf4
Thomas Cokelaer <cokelaer@gmail.com>
2008-12-11 08:43:55 +0000

    This test code was raising a seg fault because QT was called outside of a main thread.
    Using test_thread test example, I changed the code so that there is a Thread class with a method containing the previous code to prevent the seg fault.
    
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk/PlantGL@5834 ab253cce-29fb-0310-bb2f-979600cdbdeb

--- test/cast_ray2.py
+++ test/cast_ray2.py
@@ -1,8 +1,35 @@
-from pgl_import_all import *
+"""
+Test code on castRays2()
+"""
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+from openalea.plantgl.all import Scene, Viewer, Shape, Sphere, Material, Translated, Vector3, Box
+import code
 
-Viewer.start()
-s = Scene([Shape(Sphere(),Material(),1),Shape(Translated(Vector3(0,0,50),Box(Vector3(0.3,0.3,0.3)*10)),Material(),2)])
-Viewer.display(s)
-Viewer.frameGL.setSize(600,600)
-Viewer.camera.lookAt(Vector3(55,0,0),Vector3(0,0,0))
-buf = Viewer.frameGL.castRays2(s)
+
+class MyThread(QThread):
+    def __init__(self):
+        QThread.__init__(self)
+    
+    def run(self):
+        code.interact("Sub thread interpretation",local=globals())
+        QApplication.quit()
+
+    def doit(self):
+        app = QApplication([])
+        Viewer.start()
+        s = Scene([ Shape(Sphere(),Material(),1),
+                    Shape(  Translated(Vector3(0,0,50),
+                            Box(Vector3(0.3,0.3,0.3)*10)),
+                            Material((0,0,0)),2)])
+        Viewer.display(s)
+        Viewer.frameGL.setSize(600,600)
+        Viewer.camera.lookAt(Vector3(55,0,0),Vector3(0,0,0))
+        buf = Viewer.frameGL.castRays2(s)
+        self.start()
+        app.exec_()
+
+if __name__ == '__main__':
+	m = MyThread()
+	m.doit()
+
