45c5cf1f4245baf8c6816775dc9449de7431dae8
Thomas Cokelaer <cokelaer@gmail.com>
2008-12-11 13:48:50 +0000

    move the relevant code (related to import) into a test function so as to make nosetest relevant
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk/PlantGL@5855 ab253cce-29fb-0310-bb2f-979600cdbdeb

--- test/pgl_import.py
+++ test/pgl_import.py
@@ -15,6 +15,7 @@ def addpybinpath(dir_name):
 	dir_name = os.path.abspath(dir_name)
 	sys.path.insert(0,dir_name)
 
+
 if exists('../bin'):
 	"Visual 8 Binary path added"
 	addbinpath('../bin')
@@ -25,14 +26,17 @@ else:
 
 addpybinpath('../src')
 
-import openalea.plantgl.all as pgl
 
-v = pgl.Vector3.ORIGIN
-print('Vector3 : '+str(v))
-c =pgl.Cylinder()
-print(c)
-d = pgl.Discretizer()
-print(d)
+
+def test_import():
+    import openalea.plantgl.all as pgl
+
+    v = pgl.Vector3.ORIGIN
+    print('Vector3 : '+str(v))
+    c =pgl.Cylinder()
+    print(c)
+    d = pgl.Discretizer()
+    print(d)
 
 
 
