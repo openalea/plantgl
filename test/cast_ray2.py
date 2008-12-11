b522e9186549584cba2998b085bc26e1160c9889
Thomas Cokelaer <cokelaer@gmail.com>
2008-12-11 13:47:19 +0000

    Created a function (there was no function so nosetests does nothing!)
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk/PlantGL@5854 ab253cce-29fb-0310-bb2f-979600cdbdeb

--- test/cast_ray2.py
+++ test/cast_ray2.py
@@ -4,18 +4,20 @@ Test code on castRays2()
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 from openalea.plantgl.all import Scene, Viewer, Shape, Sphere, Material, Translated, Vector3, Box
-import code
+
+
+
+def test_castRays2():
+    m = MyThread()
+    m.test_doit()
+    return
 
 
 class MyThread(QThread):
     def __init__(self):
         QThread.__init__(self)
     
-    def run(self):
-        code.interact("Sub thread interpretation",local=globals())
-        QApplication.quit()
-
-    def doit(self):
+    def test_doit(self):
         app = QApplication([])
         Viewer.start()
         s = Scene([ Shape(Sphere(),Material(),1),
@@ -27,9 +29,8 @@ class MyThread(QThread):
         Viewer.camera.lookAt(Vector3(55,0,0),Vector3(0,0,0))
         buf = Viewer.frameGL.castRays2(s)
         self.start()
-        app.exec_()
+        #app.exec_()
+
 
 if __name__ == '__main__':
-	m = MyThread()
-	m.doit()
-
+    test_castRays2()
