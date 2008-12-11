325338acd28747931a76c6da536461e6bfeffc16
Thomas Cokelaer <cokelaer@gmail.com>
2008-12-11 08:52:09 +0000

    same issue as cast_ray2.py last commit : QTApplication fails if outside of the mian thread. I created a class to call the application similarly to what is done inside test_thread.py
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk/PlantGL@5836 ab253cce-29fb-0310-bb2f-979600cdbdeb

--- test/test_object_creation.py
+++ test/test_object_creation.py
@@ -1,4 +1,8 @@
-from openalea.plantgl.all import *
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+from openalea.plantgl.all import Scene, Viewer, Shape, Sphere, Material, Translated, Vector3, Box
+import code
+
 
 def create_default_objects():
     yield AmapSymbol('../share/plantgl/database/amapsymbols/nentn105.smb')
@@ -26,13 +30,29 @@ def create_default_objects():
     yield NurbsCurve2D([(0,0,1),(.5,1,1),(1.,1,1),(1.5,0,1)])
     yield NurbsPatch([[(0,0,0,1),(.5,1,0,1),(1.,1,0,1),(1.5,0,0,1)],[(0,0,1,1),(.5,1,1,1),(1.,1,1,1),(1.5,0,1,1)],[(0,0,3,1),(.5,1,3,1),(1.,1,3,1),(1.5,0,3,1)]],2,2)
 
-def test_create_default_objects():
-    s = Scene()
-    for i,geom in enumerate(create_default_objects()):
-        assert geom.isValid()
-        s += Shape(Translated((0,i*2,0),geom))
-    Viewer.display(s)
+
+class MyThread(QThread):
+    def __init__(self):
+        QThread.__init__(self)
     
-if __name__ == '__main__':
-    test_create_default_objects()
+    def run(self):
+        code.interact("Sub thread interpretation",local=globals())
+        QApplication.quit()
+
+    def doit(self):
+        app = QApplication([])
+        Viewer.start()
+    	s = Scene()
+    	for i,geom in enumerate(create_default_objects()):
+            assert geom.isValid()
+            s += Shape(Translated((0,i*2,0),geom))
         
+        Viewer.display(s)
+        
+        self.start()
+        app.exec_()
+
+
+if __name__ == '__main__':
+    m = MyThread()
+    m.doit()
