9007cbde40b771e7698223009b17c71ad7f41ecc
Thomas Cokelaer <cokelaer@gmail.com>
2008-12-11 09:10:03 +0000

    converion to pep8 convention
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk/PlantGL@5840 ab253cce-29fb-0310-bb2f-979600cdbdeb

--- versionreader.py
+++ versionreader.py
@@ -1,13 +1,19 @@
-import re, os
-pj =  os.path.join 
+import re
+import os
+
+
+pj = os.path.join
+
 
 class VersionReader:
-    def __init__(self,fname):
-        stream = file(fname,'r')
+
+    def __init__(self, fname):
+        stream = file(fname, 'r')
         self.text = stream.read()
-    def find(self,varname):
+
+    def find(self, varname):
         pattern = '#define\s+?'+varname+'\s+?(.+?)\s*?$'
-        res = re.findall(pattern,self.text,re.M)
+        res = re.findall(pattern, self.text, re.M)
         if not res is None:
             res = res[0]
             try:
@@ -15,30 +21,35 @@ class VersionReader:
             except:
                 pass
         return res
-        
+
 
 class PGLVersionReader (VersionReader):
+
     def __init__(self):
-        VersionReader.__init__(self,pj('src','cpp','plantgl','version.h'))
+        VersionReader.__init__(self, pj('src', 'cpp', 'plantgl', 'version.h'))
+
     def getVersion(self):
         return self.find('PGL_VERSION')
+
     def getRevision(self):
         return self.find('PGL_SVNREVISION')
 
-def version_str(hex_version,str_revision = None):
+
+def version_str(hex_version, str_revision = None):
     major = ((hex_version & 0xff0000) >> 16)
     minor = ((hex_version & 0x00ff00) >> 8)
-    rev   = (hex_version & 0x0000ff)
+    rev = (hex_version & 0x0000ff)
     vers_str = str(major)+'.'+str(minor)+'.'+str(rev)
-    # if not str_revision is None : 
+    # if not str_revision is None :
         # str_revision = str_revision.split(':')[1][1:-1]
         # if len(str_revision) > 0:
             # vers_str +='-r'+str_revision
     return vers_str
 
+
 def getPGLVersionString():
     p = PGLVersionReader()
-    return version_str(p.getVersion(),p.getRevision())    
+    return version_str(p.getVersion(), p.getRevision())
 
-if __name__ == '__main__':    
+if __name__ == '__main__':
     print getPGLVersionString()
