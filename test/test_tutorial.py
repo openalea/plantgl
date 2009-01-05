12cb5400e5cb7882eaeff04d80949d085b615e97
Thomas Cokelaer <cokelaer@gmail.com>
2009-01-05 15:09:38 +0000

    This test was in openalea/core/test and requires plantGL. Since openalea is built before plantGL, this testwas failing. it was decided to move it to this directory, within plantGL
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk/PlantGL@5918 ab253cce-29fb-0310-bb2f-979600cdbdeb

---
+++ test/test_tutorial.py
@@ -0,0 +1,32 @@
+
+
+from openalea.core.pkgmanager import PackageManager
+import os, sys
+from PyQt4 import QtGui
+
+def test_tutorial():
+    app = QtGui.QApplication(sys.argv)
+    
+    path = os.path.join( os.path.abspath(os.path.dirname(__file__)), "tutorial_standbuilder")
+    image = os.path.join(path, "stand.csv.png")
+    try:
+        os.remove(image)
+    except:
+        pass
+
+
+    pm = PackageManager()
+    pm.init()
+
+    pm.load_directory(path)
+    n = pm.get_node("tutorial_standbuilder", "gen_images")
+
+    print path
+    n.actor(4).set_input(0, path)
+
+# the following call create a segmentation fault
+    #assert False
+    #n()
+    #assert os.path.exists(image)
+    
+
