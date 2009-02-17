40de53dc35c22df293e5653e5cc5cc415b659fc1
Thomas Cokelaer <cokelaer@gmail.com>
2009-02-17 14:48:20 +0000

    add PlantGL material for the sphinx documentation
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk/PlantGL@6014 ab253cce-29fb-0310-bb2f-979600cdbdeb

---
+++ doc/postprocess.py
@@ -0,0 +1,38 @@
+""" clean up tool
+
+The reST files are automatically generated using sphinx_tools.
+
+However, there are known issus which require cleaning.
+
+This code is intended at cleaning these issues until a neat 
+solution is found.
+
+:known problem:
+
+- in ./deploy/openalea_deploy_binary_deps_ref.rst, the automodule 
+includes the module binary_deps.py but there is only one
+function inside this module. The automodule fails. To prevent this failure
+switch the automodule to autofunction and remove all the fields below 
+'.. autofunction::' that are not required anymore.
+"""
+import os
+import sys
+sys.path.append('../../misc')
+sys.path.append('../../../openalea/misc')
+import sphinx_tools
+
+
+
+filenames = ['PlantGL/openalea_plantgl_codec_asc_ref.rst']
+for file in filenames:
+    process = sphinx_tools.PostProcess(file)
+    process.remove_inheritance()
+
+filenames = ['PlantGL/openalea_versionreader_ref.rst']
+for file in filenames:
+    process = sphinx_tools.PostProcess(file)
+    process.no_namespace_in_automodule()
+
+
+print 'Try python setup.py build_sphinx now.'
+
