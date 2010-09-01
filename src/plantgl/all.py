c1442fc9f3a5fbd7e4ccc13eed96f3ee8a3b5b05
Christophe Godin <christophe.godin@inria.fr>
2010-09-01 12:29:26 +0000

    fix bug of all.py with shared_data and previous version of openalea
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk/PlantGL@9480 ab253cce-29fb-0310-bb2f-979600cdbdeb

--- src/plantgl/all.py
+++ src/plantgl/all.py
@@ -8,10 +8,10 @@ except:
 import codec
 
 
-from openalea.deploy.shared_data import get_shared_data_path
 from os.path import join as pj
 
 def get_shared_data(file, share_path=pj('share','plantgl', 'database')):
+    from openalea.deploy.shared_data import get_shared_data_path
     import openalea.plantgl
     shared_data_path = get_shared_data_path(openalea.plantgl.__path__, share_path=share_path)
     return pj(shared_data_path, file)
