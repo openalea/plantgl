730af3f1e883a661b5d7f77d1c5afa2da8bb2268
Daniel Barbeau <daniel.barbeau@sophia.inria.fr>
2010-09-09 12:36:46 +0000

    fixes by C. Pradal for linear cspline computation + adds possibility to specify the distances.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk/PlantGL@9528 ab253cce-29fb-0310-bb2f-979600cdbdeb

--- src/plantgl/scenegraph/cspline.py
+++ src/plantgl/scenegraph/cspline.py
@@ -37,13 +37,13 @@ class CSpline:
             elif self.dim == 4:
                 self.points = pgl.Point4Array(points)
         else:
-            self.dim = 3        
+            self.dim = 3
             self.points = points
         self.is_closed = is_closed
         if is_closed:
             if norm(points[-1]-points[0]) > epsilon:
                 self.points.append(points[0])
- 
+
         self.nurbs= None
         self.dist = None
         self.der = None
@@ -74,9 +74,9 @@ class CSpline:
         dn = (self.points[-1]-self.points[-2])/ (2.)
         if self.is_closed:
             d0 = dn = deriv(self.points[-1], self.points[0], self.points[1], self.dist[-1], self.dist[0])
-        
+
         self.der = [d0]
-        
+
         for i in xrange(1,n-1):
             p, q, r = self.points[i-1], self.points[i], self.points[i+1]
             dx1, dx2 = self.dist[i-1], self.dist[i]
@@ -90,8 +90,8 @@ class CSpline:
 
     def _derivative2( self, p, q, r, d0, d1):
         d= d0+d1
-        d0 /= d 
-        d1 /= d 
+        d0 /= d
+        d1 /= d
         der = (q-p)/(2*d0)+(r-q)/(2*d1)
         return der
 
@@ -104,14 +104,14 @@ class CSpline:
         n = len(self)
         self.ctrl_pts = []
         for i in xrange(0,n-1):
-            p, q = self.points[i:i+2] 
+            p, q = self.points[i:i+2]
             dp, dq = self.der[i:i+2]
             self.ctrl_pts.append(p)
             self.ctrl_pts.append(p+dp*a)
             self.ctrl_pts.append(q-dq*a)
         # last point
         self.ctrl_pts.append(self.points[-1])
-        
+
         if self.is_closed:
             self.ctrl_pts[1] = self.ctrl_pts[2]
             self.ctrl_pts[-2] = self.ctrl_pts[-3]
@@ -121,31 +121,32 @@ class CSpline:
         Compute a nurbs knot vector from Bezier control points.
         bezier_kv(linear=False) -> knot_vector
 
-        :param: is_linear indicate if the parametrization is linear or 
+        :param: is_linear indicate if the parametrization is linear or
         pseudo curvilinear abscisse.
         """
-        degree = 3
         nb_pts = len(self.ctrl_pts )
         nb_arc = len(self)-1
+        degree = 3
         assert nb_arc == (nb_pts-1) / degree
 
         nb_knots = degree + nb_pts
         p = 0.
         param = [p]
+
         if is_linear:
-            param = range(nb_arc)
-            self.kv= [ param[ 0 ] ]*degree
-            step = (param[-1]-param[0]) / float(((nb_arc-2)*degree+1))
-            for i in range((nb_arc-2)*degree+2):
-                self.kv.append( param[0]+i*step )
-            self.kv.extend( [param[ -1 ]]*degree )
+            param = map(float,range(nb_arc+1))
+            self.kv= [ param[ 0 ] ]*(degree+1)
+            step = (param[-1]-param[0]) / float(nb_arc)
+            for i in range(1,nb_arc):
+                self.kv.extend([param[0]+i*step]*degree)
+            self.kv.extend( [param[ -1 ]]*(degree+1) )
         else:
             dist = self.dist
             assert len(dist) == nb_arc
             for d in dist:
                 p += d
                 param.append( p )
-    
+
                 self.kv= [ param[ 0 ] ]
                 for p in param:
                     for j in range(degree):
@@ -159,7 +160,7 @@ class CSpline:
         self.points.append(pt)
         # TODO: Compute incrementally the curve.
         self.nurbs = None
-            
+
     def move_point( self, i, pt ):
         if i >= len(self):
             raise "Index %d out of range [0,%d[" % (i, len(self))
@@ -168,20 +169,23 @@ class CSpline:
         # TODO: Compute incrementally the curve.
         self.nurbs = None
 
-    def curve(self, is_linear= False, stride_factor=10):
+    def curve(self, is_linear= False, stride_factor=10, distances=None):
         """
         Return the equivalent PlantGL nurbs curve which interpol the points.
         :param: stride_factor is the number of points to draw an arc of the curve.
         """
         if not self.nurbs:
-            self.distances()
+            if distances is None:
+                self.distances()
+            else:
+                self.dist = distances
             self.derivatives()
             self.bezier_cp()
             self.bezier_kv(is_linear)
- 
+
         pts = self.points
         kv = pgl.RealArray(self.kv)
-        
+
         stride = (len(pts)-1) * stride_factor
         if self.dim == 3:
             ctrl_pts = pgl.Point4Array(self.ctrl_pts,1)
@@ -190,18 +194,18 @@ class CSpline:
             ctrl_pts = pgl.Point3Array(self.ctrl_pts,1)
             self.nurbs = pgl.NurbsCurve2D( ctrl_pts, kv, 3, stride)
         else:
-            raise "Unable to build a spline curve from points of dimension %d"% (self.dim,)
+            raise Exception("Unable to build a spline curve from points of dimension %d"% (self.dim,))
 
         return self.nurbs
-            
+
 ###############################################################################
 
 # factory function
-def cspline(pts, is_closed=False,is_linear=False):
+def cspline(pts, is_closed=False,is_linear=False,distances=None):
     """
     Build a nurbs curve by interpolate (C1) the points pts.
     The resulting curve can be closed.
     """
     spline = CSpline(pts,is_closed)
-    return spline.curve(is_linear)
+    return spline.curve(is_linear, distances=distances)
 
