7d968e68b5fa152592eb25ebe0cc7b57b03d5458
Camille Chambon <cchambon@grignon.inra.fr>
2013-11-04 16:44:12 +0000

    Add fedora 19 support.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk/PlantGL@15179 ab253cce-29fb-0310-bb2f-979600cdbdeb

--- options_linux.py
+++ options_linux.py
@@ -1,11 +1,12 @@
 import platform
 
 system = platform.system()
-if system is "Linux":
-    name, version, id = plaform.dist()
+if system == "Linux":
+    name, version, id = platform.dist()
     
     QTDIR="/usr"
     QT_VERSION=4
-    if name == 'fedora' and version =='17':
+    if name == 'fedora' and \
+        (version =='17' or version =='19'):
         ann_libs='ANN'
-	boost_libs_suffix='-mt'
+    boost_libs_suffix='-mt'
