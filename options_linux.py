262e76155655070c7fb757120ca8dca2cfe5adbe
Camille Chambon <cchambon@grignon.inra.fr>
2014-06-26 09:06:56 +0000

    Add openSUSE build service specific pre-processing.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk/PlantGL@16975 ab253cce-29fb-0310-bb2f-979600cdbdeb

--- options_linux.py
+++ options_linux.py
@@ -2,7 +2,9 @@ import platform
 
 system = platform.system()
 if system == "Linux":
-    name, version, id = platform.dist()
+    # Note: when openSUSE build service is used, you must replace manually 'OBS_DISTNAME' 
+    # and 'OBS_DISTVERSION' by the right value. 
+    name, version, id = platform.dist(distname='OBS_DISTNAME', version='OBS_DISTVERSION')
     
     QTDIR="/usr"
     QT_VERSION=4
