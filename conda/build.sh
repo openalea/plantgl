6a1282b92c2fab85959f3a3c74a5279f00c309fa
Christophe Pradal <christophe.pradal@inria.fr>
2017-11-20 13:01:20 +0100

    Update build.sh

--- conda/build.sh
+++ conda/build.sh
@@ -2,17 +2,17 @@
 
 export PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig
 
-cp options_conda.py options.py
+#cp options_conda.py options.py
 
-scons -h
+#scons -h
 
-echo "ls $PREFIX/include"
-ls $PREFIX/include
+#echo "ls $PREFIX/include"
+#ls $PREFIX/include
 
-echo "ls $PREFIX/include/qt"
-ls $PREFIX/include/qt
+#echo "ls $PREFIX/include/qt"
+#ls $PREFIX/include/qt
 
-echo "ls $PREFIX/include/qt/QtCore/"
-ls $PREFIX/include/qt/QtCore/
+#echo "ls $PREFIX/include/qt/QtCore/"
+#ls $PREFIX/include/qt/QtCore/
 
-$PYTHON setup.py scons --scons-ext-param="EXTRA_CPPPATH=$PREFIX/include/qt/QtCore WITH_LAPACK=False EXTRA_LIBS='boost_system png'" install --prefix=$PREFIX
+$PYTHON setup.py scons --scons-ext-param="WITH_LAPACK=False EXTRA_LIBS='boost_system png'" install --prefix=$PREFIX
