9028bd50727807ec1a01f04b24f211bb0d5f48e1
Simon Artzet <simon.artzet@gmail.com>
2017-11-21 16:11:59 +0100

    remove modification Qt and add scons -h and ls for debugging

--- conda/build.sh
+++ conda/build.sh
@@ -4,15 +4,20 @@ export PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig
 
 #cp options_conda.py options.py
 
-#scons -h
+echo -e "\n\n\nscons -h :"
+scons -h
+echo -e "\n\n\n\n\n"
 
-#echo "ls $PREFIX/include"
-#ls $PREFIX/include
+echo -e "\n\n\nls $PREFIX/include"
+ls $PREFIX/include
+echo -e "\n\n\n\n\n"
 
-#echo "ls $PREFIX/include/qt"
-#ls $PREFIX/include/qt
+echo "ls $PREFIX/include/qt"
+ls $PREFIX/include/qt
+echo -e "\n\n\n\n\n"
 
-#echo "ls $PREFIX/include/qt/QtCore/"
-#ls $PREFIX/include/qt/QtCore/
+echo "ls $PREFIX/include/qt/QtCore/"
+ls $PREFIX/include/qt/QtCore/
+echo -e "\n\n\n\n\n"
 
 $PYTHON setup.py scons --scons-ext-param="WITH_LAPACK=False EXTRA_LIBS='boost_system png' --debug=stacktrace " install --prefix=$PREFIX
