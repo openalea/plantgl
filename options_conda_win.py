d0a7cfb45ebde12657be9137743a7f0a192a98c0
Frederic Boudon <frederic.boudon@cirad.fr>
2017-12-20 23:31:55 +0100

    add auto detection of qtversion for window conda

--- options_conda_win.py
+++ options_conda_win.py
@@ -5,7 +5,7 @@ build_prefix="build-scons"
 
 import sys
 import os
-
+import glob
 
 compiler = 'msvc'
 
@@ -15,4 +15,12 @@ qhull_libs_suffix = 'static'
 
 if 'CPU_COUNT' in os.environ:
     num_jobs = os.environ['CPU_COUNT']
+
+try:
+    qtconfig = file(os.path.join(os.environ['LIBRARY_INC'],'qt','QtCore','qglobal.h'),'r').read()
+    pattern = '#define QT_VERSION '
+    i = qtconfig.index(pattern)+len(pattern)
+    qtconfig = qtconfig[i:].splitlines()[0]
+    QT_VERSION = eval(qtconfig) >> 16
+except:
     QT_VERSION = 5
