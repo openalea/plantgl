41f3c6350bf6ffa65a69b290c0f9b59af17609d3
Frederic Boudon <frederic.boudon@cirad.fr>
2017-12-20 23:04:13 +0100

    add qt5 in option.py for conda window

--- options_conda_win.py
+++ options_conda_win.py
@@ -14,5 +14,5 @@ EXTRA_CXXFLAGS="-DCGAL_CFG_NO_CPP0X_VARIADIC_TEMPLATES  -D_GLIBCXX_USE_CXX11_ABI
 qhull_libs_suffix = 'static'
 
 if 'CPU_COUNT' in os.environ:
-	num_jobs = os.environ['CPU_COUNT']
+    num_jobs = os.environ['CPU_COUNT']
     QT_VERSION = 5
