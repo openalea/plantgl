d9ecd0596d5fb6795b39593852e5f922ea304992
Frederic Boudon <fredboudon@users.noreply.github.com>
2017-12-28 15:12:09 +0100

    Update bfstream.cpp

--- src/cpp/plantgl/tool/bfstream.cpp
+++ src/cpp/plantgl/tool/bfstream.cpp
@@ -38,7 +38,7 @@
 
 /* ----------------------------------------------------------------------- */
 #ifdef _MSC_VER
-#pragma message("_MSC_VER:" #_MSC_VER)
+#pragma message("_MSC_VER:" ##_MSC_VER)
 #ifdef _WIN64
 #pragma message("64bit ARCHITECTURE TARGETTED")
 #else
