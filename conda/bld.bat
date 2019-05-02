e196d604d5887f24a9605abf5b4b82deb5b09e8c
Ethan Margaillan <ethan.margaillan@gmail.com>
2019-05-02 17:05:01 +0200

    Conda Build: Restore build with NMake on Windows

--- conda/bld.bat
+++ conda/bld.bat
@@ -3,14 +3,12 @@ mkdir build
 cd build
 
 :: Build
-:: cmake -G "NMake Makefiles" -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -DCMAKE_BUILD_TYPE=Release ..
-cmake .. -G "Visual Studio 14 2015 Win64" -T host=x64 -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX%
+cmake -G "NMake Makefiles" -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -DCMAKE_BUILD_TYPE=Release ..
 if errorlevel 1 exit 1
-:: nmake
-cmake --build . --target install --config Release
+nmake
 if errorlevel 1 exit 1
-:: nmake install
-::if errorlevel 1 exit 1
+nmake install
+if errorlevel 1 exit 1
 
 :: Install Python Files
 cd ..
