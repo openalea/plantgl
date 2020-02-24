46ea1855ef2551af5c58fefc982fe6b34207cfa7
Frederic Boudon <frederic.boudon@cirad.fr>
2020-02-24 15:07:21 +0100

    fix bug with zbuf test and multithread

--- test/test_zbufferengine.py
+++ test/test_zbufferengine.py
@@ -6,7 +6,7 @@ from os.path import join, dirname, abspath, exists
 
 fname = abspath(join(dirname(__file__),'data/teapot.obj'))
 
-MT = True
+MT = False
 
 def test_teapot(view = False):
     s = Scene(fname)
