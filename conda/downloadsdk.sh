21061c603707493e61c5e102cd75a46430347439
Frederic Boudon <frederic.boudon@cirad.fr>
2020-09-16 21:36:37 +0200

    local sdk download

--- conda/downloadsdk.sh
+++ conda/downloadsdk.sh
@@ -1,6 +1,5 @@
 if [ `uname` = "Darwin" ];  then 
-    cd /opt 
     wget https://github.com/phracker/MacOSX-SDKs/releases/download/10.15/MacOSX10.10.sdk.tar.xz
     tar xvzf MacOSX10.10.sdk.tar.xz
-    cd -
+    echo "CONDA_BUILD_SYSROOT:\n - `pwd`/MacOSX10.10.sdk        # [osx]" > conda/conda_build_config.yaml
 fi
