6c5e524c5ab29a1b9e7ac1db5e1752bb27dbbdd2
Frederic Boudon <frederic.boudon@cirad.fr>
2020-09-16 21:45:10 +0200

    use sdk 10.9

--- conda/downloadsdk.sh
+++ conda/downloadsdk.sh
@@ -1,5 +1,5 @@
 if [ `uname` = "Darwin" ];  then 
-    wget https://github.com/phracker/MacOSX-SDKs/releases/download/10.15/MacOSX10.10.sdk.tar.xz
-    tar xvzf MacOSX10.10.sdk.tar.xz
-    echo "CONDA_BUILD_SYSROOT:\n - `pwd`/MacOSX10.10.sdk        # [osx]" > conda/conda_build_config.yaml
+    wget https://github.com/phracker/MacOSX-SDKs/releases/download/10.15/MacOSX10.9.sdk.tar.xz
+    tar xzf MacOSX10.9.sdk.tar.xz
+    echo "CONDA_BUILD_SYSROOT:\n - `pwd`/MacOSX10.9.sdk        # [osx]" > conda/conda_build_config.yaml
 fi
