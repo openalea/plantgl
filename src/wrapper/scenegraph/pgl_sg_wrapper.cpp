4b87b03d9caf9cbfb8c433c60ef8fcbb994d7ecc
Frederic Boudon <frederic.boudon@cirad.fr>
2020-10-13 10:43:18 +0200

    set version to 3.5. Remove svn revision

--- src/wrapper/scenegraph/pgl_sg_wrapper.cpp
+++ src/wrapper/scenegraph/pgl_sg_wrapper.cpp
@@ -224,11 +224,10 @@ void module_sg()
 
     export_Plane();
 
-  export_Progress();
+    export_Progress();
 
     scope().attr("PGL_VERSION_STR") = getPGLVersionString();
     scope().attr("PGL_VERSION") = PGL_VERSION;
-    scope().attr("PGL_SVNREVISION") = getPGLSvnRevision();
     def("get_pgl_version",&getPGLVersion);
     def("get_pgl_supported_extensions",&py_get_pgl_supported_extensions,"Gives all extensions supported by current version of PlantGL.");
     def("pgl_support_extension",&pgl_support_extension, args("ext"),"Tell wether PlantGL support a given extension");
