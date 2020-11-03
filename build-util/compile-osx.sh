14a2656016fd04d316aa0261780fd5fad27a72a0
Frederic Boudon <frederic.boudon@cirad.fr>
2020-11-03 19:10:16 +0100

    set boost to v1.73 for everybody as it creates pb also for mac

--- build-util/compile-osx.sh
+++ build-util/compile-osx.sh
@@ -7,6 +7,7 @@ cmake -DCMAKE_INSTALL_PREFIX=${CONDA_PREFIX} \
       -DOPENGL_gl_LIBRARY=/Users/fboudon/Develop/SDK/MacOSX10.9.sdk/System/Library/Frameworks/OpenGL.framework \
       -DOPENGL_glu_LIBRARY=/Users/fboudon/Develop/SDK/MacOSX10.9.sdk/System/Library/Frameworks/OpenGL.framework \
       -DBoost_DEBUG=ON \
+      -DCMAKE_IGNORE_PATH=/opt/local/lib \
       -LAH ..
 
 #cmake -DCMAKE_BUILD_TYPE=Release -DBoost_INCLUDE_DIR=/opt/local/include ..
@@ -15,3 +16,7 @@ make
 make install
 cd ..
 python setup.py install
+
+{
+    
+}
