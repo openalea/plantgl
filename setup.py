3ef4788c90f7f355a9271007f265169b8fdc6791
pradal <christophe.pradal@inria.fr>
2022-04-19 14:56:30 +0200

    Remove declaration of namespace package

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ currentdir = os.path.dirname(__file__)
 build_prefix= "build-cmake"
 
 if 'CONDA_PREFIX' in os.environ or 'PREFIX' in os.environ :
-    deploy_args = dict(namespace_packages = [namespace])
+    deploy_args = dict() # Do not declare namespace_package : see https://github.com/conda/conda-build/issues/649
 else:
     deploy_args = dict(
         # Specific options of openalea.deploy
