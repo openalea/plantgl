2f847b290974a5932fe400e76e30a00e9ded8bbc
Arthur Luciani <arthur.luciani@inria.fr>
2022-06-24 09:16:38 +0200

    Always link python for boost wrapper and prefer CONDA include and lib path before system wide path
    Signed-off-by: Arthur Luciani <arthur.luciani@inria.fr>

--- cmake/PglWrapper.cmake
+++ cmake/PglWrapper.cmake
@@ -1,14 +1,11 @@
 
 
 function(pglwrapper_link_python libwrapname)
-    if(NOT APPLE OR NOT USE_CONDA)
-        if (Python3_FOUND)
-             target_link_libraries(${libwrapname} Python3::Python)
-        elseif (Python2_FOUND)
-            target_link_libraries(${libwrapname} Python2::Python)
-        endif()
-    else()
-        message(STATUS "Do not link with Python directly : " ${libwrapname})
+    if (Python3_FOUND)
+        message("Linking  python to ${libwrapname}")
+         target_link_libraries(${libwrapname} Python3::Python)
+    elseif (Python2_FOUND)
+        target_link_libraries(${libwrapname} Python2::Python)
     endif()
 endfunction()
 
