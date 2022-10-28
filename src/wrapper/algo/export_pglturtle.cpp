fbbc2595ada4214f99881247d2317c1ff4a0397d
Arthur Luciani <arthur.luciani@inria.fr>
2022-10-28 10:09:36 +0200

    setDrawer
    Signed-off-by: Arthur Luciani <arthur.luciani@inria.fr>

--- src/wrapper/algo/export_pglturtle.cpp
+++ src/wrapper/algo/export_pglturtle.cpp
@@ -102,5 +102,7 @@ void export_PglTurtle()
     .def("pglShape",    (void(PglTurtle::*)( const GeometryPtr, real_t))&PglTurtle::pglShape, "Insert a custom plantgl primitive at the turtle position and orientation", (bp::arg("geometry"),bp::arg("scale")=1), return_self<>() )
     .def("pglShape",    (void(PglTurtle::*)( const ShapePtr, real_t))&PglTurtle::pglShape, "Insert a custom plantgl primitive at the turtle position and orientation", (bp::arg("geometry"),bp::arg("scale")=1), return_self<>() )
     .def("pglShape",    (void(PglTurtle::*)( const ScenePtr, real_t))&PglTurtle::pglShape, "Insert a custom plantgl primitive at the turtle position and orientation", (bp::arg("geometry"),bp::arg("scale")=1), return_self<>() )
+    .def("getDrawer", (TurtleDrawerPtr(PglTurtle::*)()) &PglTurtle::getDrawer) //TODO: cannot find converter for this value -> to fix
+    .def("setDrawer", (void(PglTurtle::*)(TurtleDrawerPtr)) &PglTurtle::setDrawer, return_self<>())
     ;
 }
