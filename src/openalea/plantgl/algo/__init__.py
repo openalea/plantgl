f383ac20509a9ec34b5910e7e7fc693e5aea7be7
Frederic Boudon <frederic.boudon@cirad.fr>
2023-02-21 22:05:55 +0100

    Improve push/pop of custom attrib for Turtle

--- src/openalea/plantgl/algo/__init__.py
+++ src/openalea/plantgl/algo/__init__.py
@@ -6,6 +6,7 @@ def pushEvent(self):
     self._pystack.append([c(getattr(self,p)) for p,c in self._params.items()])
 
 def popEvent(self):
+    assert len(self._pystack) > 0 and "Cannot pop attributes of turtle"
     pparams = self._pystack.pop(-1)
     for (p,c),v in zip(self._params.items(),pparams):
         setattr(self, p, v)
@@ -26,25 +27,22 @@ def add_parameter(self, name, value, copy = deepcopy):
     self._params[name] = copy
 
 def remove_parameter(self, name):
-    """ Add a parameter to the Turtle that will be push and pop. For this a copy function is asked """
-    delattr(self, name, value)
+    """ Remove a parameter to the Turtle that was pushed and popped. """
+    delattr(self, name)
     if not hasattr(self,'_params'):
         self._params = {}
         self._pystack = []
         self._register_pushpop(self.pushEvent,self.popEvent)
-    else:
-        if name in self._params:
-            del self._params[name]
 
 def clear_parameters(self):
     """ Clear the python parameters of the Turtle."""
     if not hasattr(self,'_params'):
         self._register_pushpop(self.pushEvent,self.popEvent)
+        self._pystack = []
     else:
         for p,c in self._params.items():
             delattr(self,p)
     self._params = {}
-    self._pystack = []
 
 
 Turtle.add_parameter = add_parameter
@@ -52,3 +50,6 @@ del add_parameter
 
 Turtle.clear_parameters = clear_parameters
 del clear_parameters
+
+Turtle.remove_parameter = remove_parameter
+del remove_parameter
