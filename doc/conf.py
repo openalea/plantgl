716fa0ebd24047712074033bdb720312d1907f43
Frederic Boudon <frederic.boudon@cirad.fr>
2024-01-25 15:28:39 +0100

    [skip ci] update the doc

--- doc/conf.py
+++ doc/conf.py
@@ -84,6 +84,7 @@ exclude_patterns = [u'_build', 'Thumbs.db', '.DS_Store']
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
 
+add_module_names = False
 
 # -- Options for HTML output -------------------------------------------------
 
