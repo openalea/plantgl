1f7a171b085f8e1b29cb358d58811d8bbf30abc0
Christian Fournier <Christian.Fournier@inra.fr>
2025-08-21 22:53:24 +0200

    Revert "cmake: modernize Python/Boost linking helpers"
    This reverts commit d72c4cc1093f8d94c663905dcc47cb036af4de4c.
    
    Come back to best try : compile py13 on ubuntu

--- cmake/PglWrapper.cmake
+++ cmake/PglWrapper.cmake
@@ -1,77 +1,47 @@
-# --- Link with Python
+
+
 function(pglwrapper_link_python libwrapname)
     if (Python3_FOUND)
-        # Prefer the "Module" target (C API for extensions, no libpython needed on 3.13+)
-        if (TARGET Python3::Module)
-            target_link_libraries(${libwrapname} PRIVATE Python3::Module)
+        if (APPLE)
+             target_link_libraries(${libwrapname} "-undefined dynamic_lookup")
+        else()
+            target_link_libraries(${libwrapname} Python3::Python)
         endif()
-        target_include_directories(${libwrapname} PRIVATE ${Python3_INCLUDE_DIRS})
-
     elseif (Python2_FOUND)
-        if (TARGET Python2::Python)
-            target_link_libraries(${libwrapname} PRIVATE Python2::Python)
-        endif()
-        target_include_directories(${libwrapname} PRIVATE ${Python2_INCLUDE_DIRS})
-
-    else()
-        message(FATAL_ERROR "Neither Python3 nor Python2 found for target ${libwrapname}")
+        target_link_libraries(${libwrapname} Python2::Python)
     endif()
 endfunction()
 
-# --- Link with Boost
 function(pglwrapper_link_boost libwrapname)
-    # Disable Boost auto-linking (important on MSVC)
+    # Disable Boost Auto-Link
     target_compile_definitions(${libwrapname} PRIVATE BOOST_ALL_NO_LIB)
 
-    # Core Boost libs
-    target_link_libraries(${libwrapname} PRIVATE Boost::system Boost::thread)
+    target_link_libraries(${libwrapname} Boost::system Boost::thread Boost::${boost_python} Boost::dynamic_linking Boost::disable_autolinking)
 
-    # Python binding library (only if found)
-    if (TARGET Boost::${boost_python})
-        target_link_libraries(${libwrapname} PRIVATE Boost::${boost_python})
-    else()
-        message(WARNING "Boost.Python component '${boost_python}' not found, skipping")
-    endif()
-
-    # These are safe interface targets
-    if (TARGET Boost::dynamic_linking)
-        target_link_libraries(${libwrapname} PRIVATE Boost::dynamic_linking)
-    endif()
-    if (TARGET Boost::disable_autolinking)
-        target_link_libraries(${libwrapname} PRIVATE Boost::disable_autolinking)
-    endif()
 endfunction()
 
-# --- Link with Boost.Numpy (if available)
 function(pglwrapper_link_numpy libwrapname)
-    if (USE_BOOST_NUMPY AND TARGET Boost::${boost_numpy})
-        target_link_libraries(${libwrapname} PRIVATE Boost::${boost_numpy})
+    if(USE_BOOST_NUMPY)
+        target_link_libraries(${libwrapname} Boost::${boost_numpy})
     endif()
 endfunction()
 
-# --- Install wrapper libraries
+
 function(pglwrapper_install libwrapname repository)
     set_target_properties(${libwrapname} PROPERTIES PREFIX "")
 
     if (WIN32)
         set_target_properties(${libwrapname} PROPERTIES SUFFIX ".pyd")
-    else()
-        # Use ".so" on Linux and macOS for Python extension modules
+    endif()
+
+    if (APPLE)
         set_target_properties(${libwrapname} PROPERTIES SUFFIX ".so")
     endif()
 
-    install(
-        TARGETS ${libwrapname}
-        DESTINATION "${CMAKE_SOURCE_DIR}/src/openalea/plantgl/${repository}"
-    )
+    install(TARGETS ${libwrapname} DESTINATION "${CMAKE_SOURCE_DIR}/src/openalea/plantgl/${repository}")
 endfunction()
 
-# --- Install shared resources
 function(install_share sharedirectory project)
-    install(
-        DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/${sharedirectory}/
-        DESTINATION "${CMAKE_INSTALL_PREFIX}/share/${project}"
-    )
+    install(DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/${sharedirectory}/ DESTINATION "${CMAKE_INSTALL_PREFIX}/share/${project}")
 endfunction()
-
 
